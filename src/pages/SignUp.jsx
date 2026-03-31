a3bb8e44312c3ccc8dc90c0b586ccb97e0e9644b
mohamedsaed <mahamedabdirahman413@gmail.com>
2026-03-31 11:33:42 +0300

    created user signed up page functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
 import {Link } from "react-router-dom";
+
 const SignUp = () => {
+
+  //function to handle form submission
+const [formData, setFormData] = useState({});
+
+// function to manage changes of the input elements
+const handleChange = (e) => {  
+    setFormData({
+      ...formData, 
+      [e.target.id]: e.target.value 
+    })
+  }
+
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+   
+    e.preventDefault();
+      try { 
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {    "Content-Type": "application/json" },
+          body: JSON.stringify(formData)
+        });
+        const data = await res.json();
+        if(data.success===false){
+          return
+        }
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         
-    <input className="border p-3 rounded-lg" type="text" placeholder="username" id="username" />
-    <input className="border p-3 rounded-lg" type="email" placeholder="email" id="email" />
-    <input className="border p-3 rounded-lg" type="password" placeholder="password" id="password" />
+    <input onChange={handleChange} className="border p-3 rounded-lg" type="text" placeholder="username" id="username" />
+    <input onChange={handleChange} className="border p-3 rounded-lg" type="email" placeholder="email" id="email" />
+    <input onChange={handleChange} className="border p-3 rounded-lg" type="password" placeholder="password" id="password" />
     <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 uppercase" >Sign Up</button>
       </form>
       <div className="flex gap-2 mt-5 justify-between">
