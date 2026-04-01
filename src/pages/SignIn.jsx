a5d40258fc9d2a5a48af3090a36489ab478cc2c3
mohamedsaed <mahamedabdirahman413@gmail.com>
2026-04-01 10:36:07 +0300

    completed sigin functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,83 @@
+import { useState } from "react";
+import {Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+
+  //function to handle form submission
+const [formData, setFormData] = useState({});
+
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false);  
+
+
+
+// function to manage changes of the input elements
+const handleChange = (e) => {  
+    setFormData({
+      ...formData, 
+      [e.target.id]: e.target.value 
+    })
+  }
+
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+      try { 
+        setLoading(true);
+        setError(null);
+
+        // let's check form data
+        if (!(formData.email && formData.password)) {
+          toast.error("Please fill in all fields");
+          setLoading(false);
+          return; // stop further execution if validation fails
+        }
+
+
+        const res = await fetch("/api/auth/signin", {
+          method: "POST",
+          headers: {    "Content-Type": "application/json" },
+          body: JSON.stringify(formData)
+        });
+        const data = await res.json();
+        if(data.success===false){
+          setError(data.message);
+          toast.error("Login failed: " + data.message);
+          setLoading(false);
+          return
+        }
+        navigate("/");
+        
+      } catch (error) {
+        setError(error.message);
+        toast.error("Login Failed: " + error.message);
+        setLoading(false);
+        toast.error("Login Failed Again: " + error.message);
+      }
+    
+  }
   return (
-    <div>SignIn</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        
+    
+    <input onChange={handleChange} className="border p-3 rounded-lg" type="email" placeholder="email" id="email" />
+    <input onChange={handleChange} className="border p-3 rounded-lg" type="password" placeholder="password" id="password" />
+    <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 uppercase" >Login</button>
+      </form>
+      <div className="flex gap-2 mt-5 justify-between">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      <ToastContainer />
+    </div>
   )
 }
 
